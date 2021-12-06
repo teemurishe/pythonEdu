b8a9b637c5dd51a3b65af013d1a4797f51f28679
teemurishe <79781857+teemurishe@users.noreply.github.com>
2021-12-06 17:56:41 +0400

    SuperClever Bot

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
-i = 0
-while i < 10:
-    i += 1
-    print(i)
+while True:
+    in_put = str(input('your msg: '))
+    if in_put.lower() == 'hello':
+        print('Hello, sweety')
+    if in_put.lower() == 'bye':
+        print('See you soon, sweetie')
+    if in_put.lower() == 'hi':
+        print('Hie-Hie, it\'s EeOneGuy!')
+    if in_put.lower() == 'wtf':
+        print('SuperClever Bot...')
+    if in_put.lower() == 'how are you':
+        print('Wonderful, but some stupid meat peace is asking me how am I')
+    if in_put.lower() == 'aboba':
+        print('Why not amogus?..')
+    if in_put.lower() == 'msg':
+        print('нет блин звоное')
+    if in_put.lower() == 'text':
+        print('picture:')
+        print('                     ,,,,,,,,')
+        print('       ,|||````||||')
+        print(' ,,,,|||||       ||,')
+        print(',||||```````       `||')
+        print(',|||`                 |||,')
+        print('||`     ....,          `|||')
+        print('||     ::::::::          |||,')
+        print('||     :::::::\'     ||    ``|||,')
+        print('||,     :::::\'               `|||')
+        print('`||,                           |||')
+        print(' `|||,       ||          ||    ,||')
+        print('   `||                        |||`')
+        print('    ||                   ,,,||||')
+        print('    ||              ,||||||```')
+        print('   ,||         ,,|||||`')
+        print('  ,||`   ||   |||`')
+        print(' |||`         ||')
+        print(',||           ||')
+        print('||`           ||')
+        print('|||,         |||')
+        print(' `|||,,    ,|||')
+        print('   ``||||||||`')
+    else:
+        print('idk what to answer you, leatherbag')
