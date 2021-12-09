f242a1fbe4f2ec0dcd1499383edefdadc32b17b6
= <=>
2021-12-09 18:21:22 +0400

    task 3

--- lesson_2/tasks.py
+++ lesson_2/tasks.py
@@ -12,3 +12,11 @@ num2 = int(input('Enter 2nd number: '))
 for i in range(num1, num2 + 1):
     if (i % 2 != 0):
         print(i)
+
+#task 3
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+
+for i in range(num1, num2 + 1):
+    if (i % 2 == 0):
+        print(i)
