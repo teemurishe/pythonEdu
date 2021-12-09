492c17aff380213a1c8b2bb0c7ef7386f6bd8881
= <=>
2021-12-09 18:30:44 +0400

    task 4

--- lesson_2/tasks.py
+++ lesson_2/tasks.py
@@ -20,3 +20,10 @@ num2 = int(input('Enter 2nd number: '))
 for i in range(num1, num2 + 1):
     if (i % 2 == 0):
         print(i)
+
+#task 4
+#Пользователь вводит с клавиатуры два числа. Нужно показать все числа в указанном диапазоне в порядке убывания
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+for i in range(num2, num1 - 1, -1):
+    print(i)
