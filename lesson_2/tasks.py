31953ea79f01c2fd06464b2e08bb1067b32bf256
= <=>
2021-12-09 18:45:45 +0400

    task text deleted, def dIntInput created

--- lesson_2/tasks.py
+++ lesson_2/tasks.py
@@ -1,3 +1,7 @@
+def dIntInput():
+    num1 = int(input('Enter 1st number: '))
+    num2 = int(input('Enter 2nd number: '))
+
 # #task 1
 # num1 = int(input('Enter 1st number: '))
 # num2 = int(input('Enter 2nd number: '))
@@ -28,24 +32,20 @@
 # for i in range(num2, num1 - 1, -1):
 #     print(i)
 
-#task 5
-#Пользователь вводит с клавиатуры два числа. Нужно показать все нечетные числа в указанном диапазоне.
-#Если границы диапазона указаны неправильно требуется
-#произвести нормализацию границ. Например, пользователь ввел 33 и 13, требуется нормализация после которой
-#начало диапазона станет равно 13, а конец 33.
-num1 = int(input('Enter 1st number: '))
-num2 = int(input('Enter 2nd number: '))
-if num1 < num2:
-    for i in range(num1, num2 + 1):
-        if (i % 2 != 0):
-            print(i)
-elif num1 > num2:
-    for i in range(num2, num1 + 1):
-        if (i % 2 != 0):
-            print(i)
-else:
-    if num1 % 2 !=0:
-        print(num1)
-    else:
-        while True:
-            break
+# task 5
+# num1 = int(input('Enter 1st number: '))
+# num2 = int(input('Enter 2nd number: '))
+# if num1 < num2:
+#     for i in range(num1, num2 + 1):
+#         if (i % 2 != 0):
+#             print(i)
+# elif num1 > num2:
+#     for i in range(num2, num1 + 1):
+#         if (i % 2 != 0):
+#             print(i)
+# else:
+#     if num1 % 2 !=0:
+#         print(num1)
+#     else:
+#         while True:
+#             break
