b49cbc4999b0cd25adb557625bd0155e9c46888b
teemurishe <teemurishe@gmail.com>
2021-12-09 23:20:33 +0400

    task 3

--- hometasks/ht3.py
+++ hometasks/ht3.py
@@ -24,3 +24,18 @@ for i in range(num1, num2 + 1):
 	if i % 5 == 0 and i != 0:
 		num5 += 1
 print(num5)
+
+#task 3
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+for i in range(num1, num2 + 1):
+	if (i % 3 != 0) and (i % 5 != 0):
+		print(i)
+	elif (i % 3 == 0) and (i % 5 == 0):
+		print('Fizz Buzz')
+	elif (i % 3 == 0) and (i % 5 != 0):
+		print('Fizz')
+	elif (i % 3 != 0) and (i % 5 == 0):
+		print('Buzz')
+	
+	
