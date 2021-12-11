5490a2b8c1eee565992d22365b1f0839185d5d06
teemurishe <teemurishe@gmail.com>
2021-12-12 01:55:56 +0400

    task 4

--- hometasks/ht4.py
+++ hometasks/ht4.py
@@ -34,18 +34,61 @@
 #     elif degree == 7:
 #         print(num * num * num * num * num * num * num)
 
-#task 3
-#Написать программу подсчета стоимости разговора для разных мобильных операторов. Пользователь вводит
-#стоимость разговора и выбирает с какого на какой оператор он звонит. Вывести стоимость на экран.
-time = int(input('Enter please time of your talk (in minutes): ')) #didn't understand the task: enter cost, choose operators and print cost
-#probably, time of dialogue was ment in first input
-oper_out = input('Choose your operator (AbobaMobile, DungeonCom): ')
-oper_friend = input('Choose your partner\'s operator (same chose): ')
-if oper_out == oper_friend: #calls in one network has same tariffs
-    print(time * 1.5, 'GBD') #GBD - GachiBoba Dollars
-elif (oper_out == 'AbobaMobile') and (oper_friend == 'DungeonCom'):
-    print(time * 3, 'GBD')
-elif (oper_out == 'DungeonCom') and (oper_friend == 'AbobaMobile'):
-    print(time * 2.25, 'GBD')
-else:
-    print('Choose correct operator, please! In the Republic of GachiBoba there are only two operators: Aboba Mobile and DungeonCom!')
+# #task 3
+# #Написать программу подсчета стоимости разговора для разных мобильных операторов. Пользователь вводит
+# #стоимость разговора и выбирает с какого на какой оператор он звонит. Вывести стоимость на экран.
+# time = int(input('Enter please time of your talk (in minutes): ')) #didn't understand the task: enter cost, choose operators and print cost
+# #probably, time of dialogue was ment in first input
+# oper_out = input('Choose your operator (AbobaMobile, DungeonCom): ')
+# oper_partner = input('Choose your partner\'s operator (same chose): ')
+# if oper_out == oper_partner: #calls in one network has same tariffs
+#     print(time * 1.5, 'GBD') #GBD - GachiBoba Dollars
+# elif (oper_out == 'AbobaMobile') and (oper_partner == 'DungeonCom'):
+#     print(time * 3, 'GBD')
+# elif (oper_out == 'DungeonCom') and (oper_friend == 'AbobaMobile'):
+#     print(time * 2.25, 'GBD')
+# else:
+#     print('Choose correct operator, please! In the Republic of GachiBoba there are only two operators: Aboba Mobile and DungeonCom!')
+
+# task 4
+sell1 = int(input('Enter the sum of the sell for 1st manager: '))
+sell2 = int(input('Enter the sum of the sell level for 2nd manager: '))
+sell3 = int(input('Enter the sum of the sell  level for 3rd manager: '))
+print()
+
+if sell1 < 500:
+    sal1 = 200 + sell1 / 100 * 3
+elif (sell1 >= 500) and (sell1 < 1000):
+    sal1 = 200 + sell1 / 100 * 5
+elif sell1 >= 1000:
+    sal1 = 200 + sell1 / 100 * 8
+
+if sell2 < 500:
+    sal2 = 200 + sell2 / 100 * 3
+elif (sell2 >= 500) and (sell2 < 1000):
+    sal2 = 200 + sell2 / 100 * 5
+elif sell2 >= 1000:
+    sal2 = 200 + sell2 / 100 * 8
+
+if sell3 < 500:
+    sal3 = 200 + sell3 / 100 * 3
+elif (sell3 >= 500) and (sell3 < 1000):
+    sal3 = 200 + sell3 / 100 * 5
+elif sell3 >= 1000:
+    sal3 = 200 + sell3 / 100 * 8
+
+if (sal1 > sal2) and (sal1 > sal3):
+    sal1 += 200
+    print('The best worker of this month is 1st manager!')
+elif (sal2 > sal1) and (sal2 > sal3):
+    sal2 += 200
+    print('The best worker of this month is 2nd manager!')
+elif (sal3 > sal2) and (sal3 > sal1):
+    sal3 += 200
+    print('The best worker of this month is 3rd manager!')
+
+print()
+
+print('1st manager\'s salary now is ', sal1, 'USD')
+print('2nd manager\'s salary now is ', sal2, 'USD')
+print('3rd manager\'s salary now is ', sal3, 'USD')
