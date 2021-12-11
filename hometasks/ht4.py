587525fd79124546858cdfe7cdbb13e07227f5c0
teemurishe <teemurishe@gmail.com>
2021-12-11 23:26:49 +0400

    task 2

--- hometasks/ht4.py
+++ hometasks/ht4.py
@@ -1,14 +1,35 @@
-#task 1
-num = int(input('Enter first number (from 1 to 100): '))
+# #task 1
+# num = int(input('Enter first number (from 1 to 100): '))
 
-if (num < 1) or (num > 100):
-    print('Incorrect numbers! Only numbers from 1 to are allowed')
+# if (num < 1) or (num > 100):
+#     print('Incorrect numbers! Only numbers from 1 to are allowed')
+# else:
+#     if (num % 3 != 0) and (num % 5 != 0):
+#         print(num)
+#     elif (num % 3 == 0) and (num % 5 == 0):
+#         print('Fizz Buzz')
+#     elif (num % 3 == 0) and (num % 5 != 0):
+#         print('Fizz')
+#     elif (num % 3 != 0) and (num % 5 == 0):
+#         print('Buzz')
+
+#task 2
+num = int(input('Enter any number: '))
+degree = int(input('Enter degree (from 1 to 7): '))
+if (degree < 1) or (degree > 7):
+    print('Incorrect degree. Choose degree from 1 to 7')
 else:
-    if (num % 3 != 0) and (num % 5 != 0):
+    if degree == 1:
         print(num)
-    elif (num % 3 == 0) and (num % 5 == 0):
-        print('Fizz Buzz')
-    elif (num % 3 == 0) and (num % 5 != 0):
-        print('Fizz')
-    elif (num % 3 != 0) and (num % 5 == 0):
-        print('Buzz')
+    elif degree == 2:
+        print(num * num)
+    elif degree == 3:
+        print(num * num * num)
+    elif degree == 4:
+        print(num * num * num * num)
+    elif degree == 5:
+        print(num * num * num * num * num)
+    elif degree == 6:
+        print(num * num* num* num * num * num)
+    elif degree == 7:
+        print(num * num * num * num * num * num * num)
