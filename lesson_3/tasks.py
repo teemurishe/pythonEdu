019ef07be053c28935f9da08e18f19211989b000
= <=>
2021-12-13 18:55:59 +0400

    task 3

--- lesson_3/tasks.py
+++ lesson_3/tasks.py
@@ -26,20 +26,61 @@
 # num = int(input('Enter length of star line: '))
 # print(num * '*')
 
-# #task 4
+# # task 4
 # num = int(input('Enter length of your line: '))
 # sym = input('Enter symbol of line: ')
 # print(num * sym)
 
-#task 5
-num = int(input('Enter number: '))
-print(num, '* 1', '=', num * 1)
-print(num, '* 2', '=', num * 2)
-print(num, '* 3', '=', num * 3)
-print(num, '* 4', '=', num * 4)
-print(num, '* 5', '=', num * 5)
-print(num, '* 6', '=', num * 6)
-print(num, '* 7', '=', num * 7)
-print(num, '* 8', '=', num * 8)
-print(num, '* 9', '=', num * 9)
-print(num, '* 10', '=', num * 10)
+# #task 5
+# num = int(input('Enter number: '))
+# print(num, '* 1', '=', num * 1)
+# print(num, '* 2', '=', num * 2)
+# print(num, '* 3', '=', num * 3)
+# print(num, '* 4', '=', num * 4)
+# print(num, '* 5', '=', num * 5)
+# print(num, '* 6', '=', num * 6)
+# print(num, '* 7', '=', num * 7)
+# print(num, '* 8', '=', num * 8)
+# print(num, '* 9', '=', num * 9)
+# print(num, '* 10', '=', num * 10)
+
+dollar = 73.46
+euro = 82.96
+bitcoin = 3518017.30
+
+while True:
+    convert = int(input('Choose operation:\n1 - Convert USD-RUB\n2 - Convert EUR-RUB\n3 - Convert BTC-RUB\n'))
+
+    if convert == 1:
+        raw_value = int(input('Choose your value:\n1 - USD\n2 - RUB\n'))
+        if raw_value == 1:
+            value = int(input('Enter your sum: '))
+            print(value * dollar)
+        elif raw_value == 2:
+            value = int(input('Enter your sum: '))
+            print(value / dollar)
+        else:
+            print('Incorrect value chosen. Try again.')
+    elif convert == 2:
+        raw_value = int(input('Choose your value:\n1 - EUR\n2 - RUB\n'))
+        if raw_value == 1:
+            value = int(input('Enter your sum: '))
+            print(value * euro)
+        elif raw_value == 2:
+            value = int(input('Enter your sum: '))
+            print(value / euro)
+        else:
+            print('Incorrect value chosen. Try again.')
+    elif convert == 3:
+        raw_value = int(input('Choose your value:\n1 - BTC\n2 - RUB\n'))
+        if raw_value == 1:
+            value = int(input('Enter your sum: '))
+            print(value * bitcoin)
+        elif raw_value == 2:
+            value = int(input('Enter your sum: '))
+            print(value / bitcoin)
+        else:
+            print('Incorrect value chosen. Try again.')
+    
+    else:
+        print('Incorrect value chosen. Try again.')
