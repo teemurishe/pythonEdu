7b3de78a15cc79f29aa81343d83b0a7861b1e88e
= <=>
2021-12-13 18:12:29 +0400

    lists, dicts, etc

---
+++ lesson_3/main.py
@@ -0,0 +1,16 @@
+num1 = ()
+num2 = (0)
+num3 = {1, 23, 4, 67, 8, 9, 23, 2, 5}
+num4 = {1:1}
+num5 = []
+num6 = ''
+num7 = 1
+num8 = frozenset()
+
+print(num4.keys())
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+print(num5)
