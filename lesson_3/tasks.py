486f15e4795e676a5c59fe888d39d116798d3281
= <=>
2021-12-13 18:15:10 +0400

    really task 1

--- lesson_3/tasks.py
+++ lesson_3/tasks.py
@@ -2,3 +2,10 @@
 num1 = int(input('Enter 1st number: '))
 num2 = int(input('Enter 2nd number: '))
 num3 = 0
+
+print((num1 + num2) / 2)
+print()
+for i in range(num1, num2 + 1):
+    num3 += i
+
+print(num3)
