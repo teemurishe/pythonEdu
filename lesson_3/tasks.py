fbfbc67ea1f525a3aa9efc14f4e771dcaa3bb942
= <=>
2021-12-13 18:33:48 +0400

    task 5

--- lesson_3/tasks.py
+++ lesson_3/tasks.py
@@ -1,32 +1,45 @@
- #task 1
-num1 = int(input('Enter 1st number: '))
-num2 = int(input('Enter 2nd number: '))
-num3 = 0
+#  #task 1
+# num1 = int(input('Enter 1st number: '))
+# num2 = int(input('Enter 2nd number: '))
+# num3 = 0
 
-print((num1 + num2) / 2)
-print()
-for i in range(num1, num2 + 1):
-    num3 += i
+# print((num1 + num2) / 2)
+# print()
+# for i in range(num1, num2 + 1):
+#     num3 += i
 
-print(num3)
+# print(num3)
 
-#task 2
-num = int(input('Enter any positive number: '))
-fact = 1
+# #task 2
+# num = int(input('Enter any positive number: '))
+# fact = 1
 
-if num > 0:
-    for i in range(1, num + 1):
-        fact *= i
-else:
-    print('Only positive numbers allowed!')
+# if num > 0:
+#     for i in range(1, num + 1):
+#         fact *= i
+# else:
+#     print('Only positive numbers allowed!')
 
-print(fact)
+# print(fact)
 
-#task 3
-num = int(input('Enter length of star line: '))
-print(num * '*')
+# #task 3
+# num = int(input('Enter length of star line: '))
+# print(num * '*')
 
-#task 4
-num = int(input('Enter length of your line: '))
-sym = input('Enter symbol of line: ')
-print(num * sym)
+# #task 4
+# num = int(input('Enter length of your line: '))
+# sym = input('Enter symbol of line: ')
+# print(num * sym)
+
+#task 5
+num = int(input('Enter number: '))
+print(num, '* 1', '=', num * 1)
+print(num, '* 2', '=', num * 2)
+print(num, '* 3', '=', num * 3)
+print(num, '* 4', '=', num * 4)
+print(num, '* 5', '=', num * 5)
+print(num, '* 6', '=', num * 6)
+print(num, '* 7', '=', num * 7)
+print(num, '* 8', '=', num * 8)
+print(num, '* 9', '=', num * 9)
+print(num, '* 10', '=', num * 10)
