c7d645ec9aaf74927b39d82eabcdcd846c947620
= <=>
2021-12-13 18:15:28 +0400

    task 2

--- lesson_3/tasks.py
+++ lesson_3/tasks.py
@@ -9,3 +9,15 @@ for i in range(num1, num2 + 1):
     num3 += i
 
 print(num3)
+
+#task 2
+num = int(input('Enter any positive number: '))
+fact = 1
+
+if num > 0:
+    for i in range(1, num + 1):
+        fact *= i
+else:
+    print('Only positive numbers allowed!')
+
+print(fact)
