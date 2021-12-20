b43ec0b86dc787e357178d1b025bea60c8892d10
= <=>
2021-12-20 18:44:10 +0400

    task 2

--- lesson_4/tasks.py
+++ lesson_4/tasks.py
@@ -1,20 +1,30 @@
-#task 1
-while True:
-    num = input('Enter any number: ')
-    numLength = len(num)
-    sumDigs = 0
-    averDigs = 0
-    sumZero = 0
-    oper = input('Select operation (1 - Sum of digits, 2 - Average of digits, 3 - How many zeros in your number): ')
-    if oper == '1':
-        for i in num:
-            sumDigs += int(i)
-        print(sumDigs)
-    elif oper == '2':
-        averDigs = sumDigs / numLength
-        print(averDigs)
-    elif oper == '3':
-        for i in num:
-            if i == '0':
-                sumZero += 1
-        print(sumZero)
+# #task 1
+# while True:
+#     num = input('Enter any number: ')
+#     numLength = len(num)
+#     sumDigs = 0
+#     averDigs = 0
+#     sumZero = 0
+#     oper = input('Select operation (1 - Sum of digits, 2 - Average of digits, 3 - How many zeros in your number): ')
+#     if oper == '1':
+#         for i in num:
+#             sumDigs += int(i)
+#         print(sumDigs)
+#     elif oper == '2':
+#         averDigs = sumDigs / numLength
+#         print(averDigs)
+#     elif oper == '3':
+#         for i in num:
+#             if i == '0':
+#                 sumZero += 1
+#         print(sumZero)
+
+#task 2
+length = int(input('Enter length of a square for chess: '))
+square0 = '\u25a1'
+square1 = '\u25a0'
+for i in range(4):
+    for i in range(length):
+        print(square1 * length, square0 * length, square1 * length, square0 * length, square1 * length, square0 * length, square1 * length, square0 * length)
+    for i in range(length):
+        print(square0 * length, square1 * length, square0 * length, square1 * length, square0 * length, square1 * length, square0 * length, square1 * length)
