8206d57daecdcdf7de2db7ae5cc6a5dd52408d3b
= <=>
2021-12-20 17:05:47 +0400

    fix variables, resolving the lower upper problem

--- lesson_4/main.py
+++ lesson_4/main.py
@@ -1,4 +1,5 @@
 str1 = 'Hello world!'
 print(str1.endswith('d'))
-print(str.startswith('Hell')) #true
-print(str.startswith('hell')) #false, cause the first letter is lower
+print(str1.startswith('Hell')) #true
+print(str1.startswith('hell')) #false, cause the first letter is lower
+print(str1.lower().startswith('hell')) #resolving the problem
