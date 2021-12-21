7b553a73a4e4f5ec3f17ec2ef12aa591b0a66b8d
teemurishe <teemurishe@gmail.com>
2021-12-21 23:25:54 +0400

    ht8 done

---
+++ hometasks/ht8.py
@@ -0,0 +1,45 @@
+# The drawings are replaced by Unicode emojis:
+# a - 🞎
+# b - 🞐
+# c - ⧠
+# d - 🞓
+# e - ⧈
+# f - 🟗
+# g - ▦
+# h - ▩
+# i - 🞖
+# j - ▤
+print('Choose an emodji to print:')
+print('a - 🞎')
+print('b - 🞐')
+print('c - ⧠')
+print('d - 🞓')
+print('e - ⧈')
+print('f - 🟗')
+print('g - ▦')
+print('h - ▩')
+print('i - 🞖')
+print('j - ▤')
+emodji = input()
+if emodji == 'a':
+    print('🞎')
+if emodji == 'b':
+    print('🞐')
+if emodji == 'c':
+    print('⧠')
+if emodji == 'd':
+    print('🞓')
+if emodji == 'e':
+    print('⧈')
+if emodji == 'f':
+    print('🟗')
+if emodji == 'g':
+    print('▦')
+if emodji == 'h':
+    print('▩')
+if emodji == 'i':
+    print('🞖')
+if emodji == 'j':
+    print('▤')
+else:
+    print('Incorrect choice. Please, select from A to J.')
