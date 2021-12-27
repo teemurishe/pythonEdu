851cd812e765f716e5007f9236e0cb868b378104
= <=>
2021-12-27 17:15:38 +0400

    fuck this stupid shit

--- lesson_5/main.py
+++ lesson_5/main.py
@@ -6,6 +6,19 @@
 # ■ Посчитайте сколько раз цифры встречаются в тексте;
 # ■ Посчитайте сколько раз знаки препинания встречаются в тексте;
 # Результат вывести на экран.
+fishText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. (1.10.32/1.10.33)'
+print('Choose operation:')
+print('1. All first letters are upper')
+print('2. How many numbers are found in the text')
+print('3. How many symbols are found in the text')
+oper = int(input('Your choice: '))
+if oper == 1:
+    fishList = fishText.split()
+    for i in range(len(fishList)):
+        str(fishList[i].capitalize())
+        print(fishList[i])
+    fishText = ' '.join(fishList)
+    print(fishText) #fuckin stupid shit of stupid shit
 
 # Задание 2
 
