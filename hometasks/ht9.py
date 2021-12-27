c942817d7ac44704d98002daecc4e7136c321c57
teemurishe <teemurishe@gmail.com>
2021-12-27 23:08:03 +0400

    comments removed

--- hometasks/ht9.py
+++ hometasks/ht9.py
@@ -1,22 +1,22 @@
-# #task 1
-# phrase = input('Enter any phrase: ')
-# noSpaces = phrase.replace(' ', '')
-# if noSpaces == noSpaces[::-1]:
-#     print('The phrase is palindrome')
-# else:
-#     print('The phrase isn\'t palindrome')
+#task 1
+phrase = input('Enter any phrase: ')
+noSpaces = phrase.replace(' ', '')
+if noSpaces == noSpaces[::-1]:
+    print('The phrase is palindrome')
+else:
+    print('The phrase isn\'t palindrome')
 
-# #task 2
-# phrase = input('Enter any phrase: ')
-# keyWords = input('Enter keywords without\',\': ')
-# keyList = keyWords.lower().split()
-# phraseList = phrase.lower().split()
-# for i in range(len(phraseList)):
-#     for a in range(len(keyList)):
-#         if keyList[a] == phraseList[i]:
-#             phraseList[i] = phraseList[i].upper()
+#task 2
+phrase = input('Enter any phrase: ')
+keyWords = input('Enter keywords without\',\': ')
+keyList = keyWords.lower().split()
+phraseList = phrase.lower().split()
+for i in range(len(phraseList)):
+    for a in range(len(keyList)):
+        if keyList[a] == phraseList[i]:
+            phraseList[i] = phraseList[i].upper()
 
-# print(' '.join(phraseList))
+print(' '.join(phraseList))
 
 #task 3
 phrase = input('Enter any phrase: ')
