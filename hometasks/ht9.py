cd19f53bd261713c91dbe513e273af2db9727dff
teemurishe <teemurishe@gmail.com>
2021-12-27 22:59:22 +0400

    it's finally done...

--- hometasks/ht9.py
+++ hometasks/ht9.py
@@ -1,19 +1,30 @@
-#task 1
-phrase = input('Enter any phrase: ')
-noSpaces = phrase.replace(' ', '')
-if noSpaces == noSpaces[::-1]:
-    print('The phrase is palindrome')
-else:
-    print('The phrase isn\'t palindrome')
+# #task 1
+# phrase = input('Enter any phrase: ')
+# noSpaces = phrase.replace(' ', '')
+# if noSpaces == noSpaces[::-1]:
+#     print('The phrase is palindrome')
+# else:
+#     print('The phrase isn\'t palindrome')
 
-#task 2
+# #task 2
+# phrase = input('Enter any phrase: ')
+# keyWords = input('Enter keywords without\',\': ')
+# keyList = keyWords.lower().split()
+# phraseList = phrase.lower().split()
+# for i in range(len(phraseList)):
+#     for a in range(len(keyList)):
+#         if keyList[a] == phraseList[i]:
+#             phraseList[i] = phraseList[i].upper()
+
+# print(' '.join(phraseList))
+
+#task 3
 phrase = input('Enter any phrase: ')
-keyWords = input('Enter keywords without\',\': ')
-keyList = keyWords.lower().split()
+point = '.'
 phraseList = phrase.lower().split()
+sentNum = 0
 for i in range(len(phraseList)):
-    for a in range(len(keyList)):
-        if keyList[a] == phraseList[i]:
-            phraseList[i] = phraseList[i].upper()
-
-print(' '.join(phraseList))
+    pointIs = '.' in phraseList[i]
+    if pointIs == True:
+        sentNum += 1
+print(sentNum)
