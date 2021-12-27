ab658ac28c81ab8770798f576de7d9dd9aa8d552
teemurishe <teemurishe@gmail.com>
2021-12-27 22:15:00 +0400

    TASK 2 DONE AAAH

--- hometasks/ht9.py
+++ hometasks/ht9.py
@@ -1,9 +1,19 @@
-#Пользователь вводит с клавиатуры строку. Проверьте является ли введенная строка палиндромом.
-#Палиндром — слово или текст, которое читается одинаково слева направо и справа налево. Например, кок; А роза
-#упала на лапу Азора; доход; А буду я у дуба
+#task 1
 phrase = input('Enter any phrase: ')
 noSpaces = phrase.replace(' ', '')
 if noSpaces == noSpaces[::-1]:
     print('The phrase is palindrome')
 else:
     print('The phrase isn\'t palindrome')
+
+#task 2
+phrase = input('Enter any phrase: ')
+keyWords = input('Enter keywords without\',\': ')
+keyList = keyWords.lower().split()
+phraseList = phrase.lower().split()
+for i in range(len(phraseList)):
+    for a in range(len(keyList)):
+        if keyList[a] == phraseList[i]:
+            phraseList[i] = phraseList[i].upper()
+
+print(' '.join(phraseList))
