2408dbf076cd79791b612a30a94bb2522c5e24e4
teemurishe <teemurishe@gmail.com>
2022-01-16 10:17:52 +0400

    task 2

--- hometasks/ht11.py
+++ hometasks/ht11.py
@@ -1,2 +1,20 @@
-#task 1
-print(eval(input('Enter your operation: '))) #AAAHAHAHAHHAHAHAHAHAHHA
+import random as rand
+# task 1
+# print(eval(input('Enter your operation: '))) #AAAHAHAHAHHAHAHAHAHAHHA
+
+#task 2
+randList = []
+for i in range(1, rand.randint(1, 100)):
+    randList.append(rand.randint(-100, 100))
+posNum = 0
+negNum = 0
+zeroNum = 0
+for i in randList:
+    if i > 0:
+        posNum += 1
+    elif i < 0:
+        negNum += 1
+    elif i == 0:
+        zeroNum += 1
+print(randList)
+print('Positive numbers: ', posNum, '\n', 'Negative numbers: ', negNum, '\n', 'Zeros: ', zeroNum)
